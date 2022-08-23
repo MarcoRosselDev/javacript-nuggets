a5b431420bd84eb8c32911783fc6a9f4db0d4c30
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-23 05:45:52 -0400

    dynamic object keys 1.5

--- 03-dynamic-object-keys/index.js
+++ 03-dynamic-object-keys/index.js
@@ -4,15 +4,15 @@ const person = {
 };
 console.log(person.name);
 person.age = 24;
-console.log(person);
+console.log(person); // { name: 'john', age: 24 }
 
 // squere bracket notation
 
 const item = {
   'feature-items': ['item1', 'item2'], 
 };
-console.log(item["feature-items"]);
-console.log(person["name"]);
+console.log(item["feature-items"]); // ['item1', 'item2']
+console.log(person["name"]); // john
 
 let appState = 'loading';
 let appState2 = 'started';
@@ -21,12 +21,13 @@ const app = {
   [appState]: true,
   [appState2]: false
 }
-console.log(app);
+console.log(app); // { loading: true, started: false}
 // con esto se puede cambiar el nombre clave de un objeto
 // bastante util
 
 app[keyName] = 'apple';
-console.log(app);
+console.log(app); /* { loading: true,
+started: false, banana: 'apple'}*/ 
 
 const state = {
   loading: true,
@@ -39,4 +40,4 @@ const updateState = (key, value) => state[key] = value;
 updateState('name', 'marco');
 updateState('job', 'arquitect & developer');
 
-console.log(state);
+console.log(state); // {loading: true, name: 'marco', job: 'arquitect & developer'}
