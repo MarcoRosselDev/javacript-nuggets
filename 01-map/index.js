3e957b215a4f916f437920ee3ea6bb00340bb693
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-22 21:56:50 -0400

    map-1

---
+++ 01-map/index.js
@@ -0,0 +1,10 @@
+// MAP Method
+
+const people = [
+
+];
+
+// Quokka.js - Extension
+// return a new array
+// does not change the size of original array (unlike filter)
+// uses values from original array when making new one
