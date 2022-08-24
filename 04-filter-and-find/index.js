9c312e3c9d3c225b01c3e436a9f0826a2d7b3671
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-23 22:12:37 -0400

    filter and find 1

---
+++ 04-filter-and-find/index.js
@@ -0,0 +1,18 @@
+// Filter - returns a new array, can manipulate the size of new array (unlike map), return based on condition
+// Find - returns single instance (object), returns first match, if no match - undefined
+
+const people = [
+  { name: 'bob', age: 20, position: 'developer'},
+  { name: 'peter', age: 25, position: 'designer'},
+  { name: 'susy', age: 30, position: 'the boss'},
+  { name: 'anna', age: 35, position: 'intern'},
+];
+// filter
+
+// no match
+
+// find
+
+// no match
+
+// multiple matches - first match
