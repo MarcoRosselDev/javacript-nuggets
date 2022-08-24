f4e2214981bf6ef8a34addcb092a19c7d8dc3a7e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-24 08:32:43 -0400

    reduce-basic 2

--- 05-reduce-basics/index.js
+++ 05-reduce-basics/index.js
@@ -9,4 +9,12 @@ const staff = [
   { name: 'peter', age: 25, position: 'designer', salary: 300},
   { name: 'susy', age: 30, position: 'the boss', salary: 400},
   { name: 'anna', age: 35, position: 'intern', salary: 10},
-]
+];
+
+const sumaTotal = staff.reduce((total,person) => {
+  console.log(total)// 0, 100, 400, 800
+  console.log(person.salary)// 100, 300, 400, 10
+  total += person.salary
+  console.log(total)// 100, 400, 800, 810  
+    return total // this part is really important
+}, 0) // and this 0 too.
