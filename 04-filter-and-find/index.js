50e2fa002fe712b6a2fcc998af2e156b2c8e58a8
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-24 03:38:21 -0400

    filter and find 1.5

--- 04-filter-and-find/index.js
+++ 04-filter-and-find/index.js
@@ -7,16 +7,23 @@ const people = [
   { name: 'susy', age: 30, position: 'the boss'},
   { name: 'anna', age: 35, position: 'intern'},
 ];
-// filter
+
+
+// filter --------------------------------------------
 const youngPeople = people.filter((a) => a.age < 30);
 console.log(youngPeople); 
 // [ { name: 'bob', age: 20, position: 'developer'},{ name: 'peter', age: 25, position: 'designer'} ];
-// no match
 const developers = people.filter((item)=> item.position === 'developer');
 console.log(developers);
-// [{ name: 'bob', age: 20, position: 'developer'}]
-// find
+// [{ name: 'bob', age: 20, position: 'developer'} ]
 
-// no match
+
+// no match ------------------------------------------
+const senirDevs = people.filter((item) => item.position === 'seniorDev')
+console.log(senirDevs);
+// []   retorna un objeto vacio.
+// find ----------------------------------------------
+
+// no match ------------------------------------------
 
 // multiple matches - first match
