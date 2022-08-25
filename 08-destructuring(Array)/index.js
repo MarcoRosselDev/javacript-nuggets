79006f2fe1269012f4ebc5b1a9a37dc1da202993
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-25 05:00:34 -0400

    destructuration array 1

---
+++ 08-destructuring(Array)/index.js
@@ -0,0 +1,10 @@
+// faster/easier way to access/unpack variables from arrays
+
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
+
+const fruit1 = fruits[0];
+const fruit2 = fruits[1];
+const fruit3 = fruits[2];
+
+console.log(fruit1, fruit2, fruit3);
