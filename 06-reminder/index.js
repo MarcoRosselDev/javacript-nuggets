00da289e114cdc6cecd50e07c3000591925f3e96
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-24 20:27:50 -0400

    reminder how to manipulate DOM and more things 5

--- 06-reminder/index.js
+++ 06-reminder/index.js
@@ -8,7 +8,21 @@ const ipt = document.querySelector('.ipt');
 const btnPut = document.querySelector('.btn-put');
 const btnClean = document.querySelector('.btn-clean');
 const lis = document.querySelector('.lis');
+const areLocalStorage = JSON.parse(localStorage.getItem('example'));
 let saveArr = [];
+
+if (areLocalStorage) {
+  saveArr = areLocalStorage;
+  printDOM(saveArr);
+}
+
+function printDOM(arr) {
+  let printHtml = '';
+  for (let i = 0; i < arr.length; i++) {
+    printHtml += `<li>${arr[i]}</li>`
+  }
+  lis.innerHTML = printHtml;
+}
 
 btnPut.addEventListener('click', () => {
   saveArr.push(ipt.value);
@@ -17,10 +31,3 @@ btnPut.addEventListener('click', () => {
   printDOM(saveArr)
 });
 
-printDOM = (arr) => {
-  let printHtml = '';
-  for (let i = 0; i < arr.length; i++) {
-    printHtml += `<li>${arr[i]}</li>`
-  }
-  return lis.innerHTML = printHtml;
-}
