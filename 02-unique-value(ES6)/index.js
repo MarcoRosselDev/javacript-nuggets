233e4d6f55e5a7eb2a483cde5081580fffe24f41
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-14 22:14:44 -0300

    coment nugget js

--- 02-unique-value(ES6)/index.js
+++ 02-unique-value(ES6)/index.js
@@ -1,40 +1,41 @@
 const menu = [
   {
-    name: 'pancakes',
-    category: 'breakfast'
+    name: "pancakes",
+    category: "breakfast",
   },
   {
-    name: 'burger',
-    category: 'lunch'
+    name: "burger",
+    category: "lunch",
   },
   {
-    name: 'steak',
-    category: 'dinner'
+    name: "steak",
+    category: "dinner",
   },
   {
-    name: 'bacon',
-    category: 'breakfast'
+    name: "bacon",
+    category: "breakfast",
   },
   {
-    name: 'eggs',
-    category: 'breakfast'
+    name: "eggs",
+    category: "breakfast",
   },
   {
-    name: 'pasta',
-    category: 'dinner'
-  }
-]
+    name: "pasta",
+    category: "dinner",
+  },
+];
 
 // map - get all instances
 // new Set - narrow down
 // ['all', ...] - turn it  back to array
 
 const categories = ["all", ...new Set(menu.map((item) => item.category))];
-console.log(categories);
+console.log(categories); // ['all', 'breakfast', 'lunch', 'dinner']
 
-const result = document.querySelector('.div');
+const result = document.querySelector(".div");
 
-result.innerHTML = categories.map((item) => {
-  return `<button>${item}</button>`
-})
-.join("");
+result.innerHTML = categories
+  .map((item) => {
+    return `<button>${item}</button>`;
+  })
+  .join("");
