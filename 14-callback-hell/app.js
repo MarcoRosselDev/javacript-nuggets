d06daa95b83a380eeebf94698538a86131fb960a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-15 23:04:17 -0300

    coment nugget callback hell 1.2

--- 14-callback-hell/app.js
+++ 14-callback-hell/app.js
@@ -1,1 +1,8 @@
 // Javascript Nuggets - Callback Hell
+
+// after 1s first red;
+// after 3s second blue;
+// after 2s third green;
+// IN SEQUENCE !!!
+
+const first = document.querySelector(".first");
