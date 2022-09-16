05d0c9e2a702e5354fd03c63d1d99332c9dfbf1d
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-16 08:24:55 -0300

    nugget async-await 1.2

--- 17-async-await/index.js
+++ 17-async-await/index.js
@@ -3,4 +3,20 @@
 // async must be present, always returns a promise
 // await waits till promise is settled
 // error handling - try/catch block
-//
+
+const users = [
+  { userId: 1, articles: ["one", "two", "three"] },
+  { userId: 2, articles: ["four", "five"] },
+  { userId: 3, articles: ["six", "seven", "eight", "nine"] },
+];
+
+function getUser(name) {
+  return new Promise((resolve, reject) => {
+    const user = users.find((user) => user.name === name);
+    if (user) {
+      return resolve(user);
+    } else {
+      reject(`No such user with name : ${name}`);
+    }
+  });
+}
