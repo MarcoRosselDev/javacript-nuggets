e58721c30a8baf0df3f94c00c0a6b1ce50d6f65d
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-16 00:07:34 -0300

    nugget primeses 1.3

--- 15-promises/index.js
+++ 15-promises/index.js
@@ -4,3 +4,10 @@
 // consume/use promises
 
 // Pending, Rejected, FulFilled
+
+const value = 2;
+
+const promise = new Promise((resolve, reject) => {
+  const random = Math.floor(Math.random() * 3);
+  console.log(random);
+});
