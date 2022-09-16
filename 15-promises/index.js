31850158513027a113fb396097060c54deb67d80
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-16 00:10:34 -0300

    nugget primeses 1.4

--- 15-promises/index.js
+++ 15-promises/index.js
@@ -10,4 +10,14 @@ const value = 2;
 const promise = new Promise((resolve, reject) => {
   const random = Math.floor(Math.random() * 3);
   console.log(random);
+  if (random === value) {
+    resolve("you guessed correctly");
+  } else {
+    reject("wrong number");
+  }
 });
+
+console.log(promise);
+
+promise.then((a) => console.log(a));
+promise.catch((err) => console.log(err));
