18aa8439fda300595eaa071a67a499b3456988d4
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-16 08:38:45 -0300

    nugget async-await 1.5

--- 17-async-await/index.js
+++ 17-async-await/index.js
@@ -4,13 +4,22 @@
 // await waits till promise is settled
 // error handling - try/catch block
 
-const example = async () => {
-  return "hello world";
-};
+// const example = async () => {
+//   return "hello world";
+// };
 
-console.log(example);
+// async function someFunc() {
+//   const result = await example();
+//   console.log(result);
+// }
 
 const users = [
+  { id: 1, name: "john" },
+  { id: 2, name: "susan" },
+  { id: 3, name: "anna" },
+];
+
+const articles = [
   { userId: 1, articles: ["one", "two", "three"] },
   { userId: 2, articles: ["four", "five"] },
   { userId: 3, articles: ["six", "seven", "eight", "nine"] },
