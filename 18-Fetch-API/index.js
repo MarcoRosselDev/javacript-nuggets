a7a0510d20da1c08263b00ceb619b14ac50f26c6
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-17 01:58:00 -0300

    nugget fetch api 1.1

---
+++ 18-Fetch-API/index.js
@@ -0,0 +1,9 @@
+// Javascript Nuggets
+
+// Fetch API - Browser API for HTML (AJAX) Requests
+// Default - GET Requests, supports other methods as well
+// Returns Promise
+
+const url = "https://www.course-api.com/react-tours-project";
+
+console.log(fetch(url));
