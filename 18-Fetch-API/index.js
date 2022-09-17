4f10ff25234f34ea3694db3a144798b34890ea41
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-17 02:11:06 -0300

    nugget fetch api 1.3

--- 18-Fetch-API/index.js
+++ 18-Fetch-API/index.js
@@ -7,8 +7,23 @@
 const url = "https://www.course-api.com/react-tours-project";
 
 console.log(fetch(url));
-
+/*
 fetch(url)
   .then((a) => a.json())
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
+*/
+
+// async await metod
+
+const getTours = async () => {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getTours();
